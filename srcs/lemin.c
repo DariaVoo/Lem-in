15bced85d225f83d60f7778df2c829b004153d8d
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-07-27 18:05:13 +0300

    ft_strstr.c is safe

--- srcs/lemin.c
+++ srcs/lemin.c
@@ -42,7 +42,8 @@ void print_paths(t_path *paths)
 	ft_printf("PATHS:\n");
 	while (paths) {
 		ft_printf("\npath%d:\t", i);
-		print_path(paths->path, paths->length);
+		ft_printf("\tlen %d\t", paths->length);
+		//print_path(paths->path, paths->length);
 		paths = paths->next;
 		i++;
 	}
