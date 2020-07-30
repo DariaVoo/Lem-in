d0066574fc2aeabd6e0c603bfbd7b543e02d2ec1
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-07-30 17:44:10 +0300

    norme move_ants.c

--- srcs/output/move_ants.c
+++ srcs/output/move_ants.c
@@ -12,7 +12,7 @@
 
 #include "lemin.h"
 
-int move_ants(t_path *path, t_room *graph)
+int	move_ants(t_path *path, t_room *graph)
 {
 	int i;
 	int j;
@@ -23,7 +23,7 @@ int move_ants(t_path *path, t_room *graph)
 	i = path->last_ant + 1;
 	if (j == -1 || path->ants[j] == 0)
 		return (0);
-	if (i == path->length) // сейчас кто-то перейдёт на финиш
+	if (i == path->length)
 	{
 		i--;
 		j--;
@@ -31,38 +31,11 @@ int move_ants(t_path *path, t_room *graph)
 	}
 	while (j != -1 && path->ants[j] != 0)
 	{
-		path->ants[i] = path->ants[j];
+		path->ants[i] = path->ants[j--];
 		ft_printf("L%d-%s ", path->ants[i], graph[path->path[i]].name);
 		i--;
-		j--;
 	}
 	path->ants[i] = 0;
-	if (!finish)
-		path->last_ant++;
-	return (finish);
-}
-
-
-
-int move_antsssssss(t_path *path)
-{//Это тоже самое, только с одной переменной, но вычисляться из-за этого будет дольше
-	int i;
-	int finish;
-
-	finish = 0;
-	i = path->last_ant + 1;
-	if (i == -1 || path->ants[i] == 0)
-		return (0);
-	if (i + 1 == path->length)
-		finish = 1;
-	while (i != -1 && path->ants[i] != 0)
-	{
-		path->ants[i + 1] = path->ants[i];
-		if (i != path->length)
-			ft_printf("L%d-%d ", path->ants[i + 1], path->path[i + 1]);
-		i--;
-	}
-	path->ants[i + 1] = 0;
 	if (!finish)
 		path->last_ant++;
 	return (finish);
