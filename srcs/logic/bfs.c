afa33b9b7470998f671eee3d5290288a3cc42b28
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-08-01 21:03:19 +0300

    norme bfs

--- srcs/logic/bfs.c
+++ srcs/logic/bfs.c
@@ -27,7 +27,7 @@ int	bfs(t_room *graph, size_t count_v, int *distance, int *queue)
 	{
 		u = queue[head++];
 		j = 0;
-		while (j < graph[u].ed_num) // смотрим все рёбра этой вершины
+		while (j < graph[u].ed_num)
 		{
 			v = graph[u].edges[j++];
 			if (distance[v] == 0 && v != 0)
